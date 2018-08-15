65a6c15e71f5bfca932a42f3862735d2aca94090
HarethNaji <hazzaldo@hotmail.com>
2018-08-15 19:00:43 +0100

    test blockchain to output log first created block with no transaction inside it

--- dev/test.js
+++ dev/test.js
@@ -15,4 +15,4 @@ testcoin.createNewTransaction(100, 'ALEXSDF34R32Q', 'JENN2343FWDCADSC');
 testcoin.createNewBlock(12121221, 'ererereer', 'sdssdsdsdsds');
 //running 'node test.js' will print Blockchain with empty chain 
 //and empty newTrasnactions
-console.log(testcoin.chain[1]);
+console.log(testcoin);
