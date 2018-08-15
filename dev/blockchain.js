f480099461dffd0941f486a07168342a3fbdc23c
Hareth Naji <hareth.naji@mindera.com>
2018-08-15 04:29:04 +0100

    add createNewBlock getLastBlock and createNewTransaction methods along with their unit tests

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -17,5 +17,63 @@ function Blockchain() {
 	//all blocks that we create and mine will be stored in this array as chain
 	this.chain = [];
 	//here we will hold all the newly created trasactions before they're placed inside a block that is mined
-	this.newTransactions = [];
+	this.pendingTransactions = [];
 }
+
+//a function to create new block on our Blockchain prototype object. 
+Blockchain.prototype.createNewBlock = function(nonce, previousBlockHash, hash) {
+	//All the data that we need is going to be stored nside of this block
+	const newBlock = {
+		//Block number - i.e. what number of block this is in our chain
+		index: this.chain.length + 1,
+		//timestamp - because we want to know when the block was created
+		timestamp: Date.now(),
+		//when we create a new block, we are going to want to put all of the new transactions 
+		//or all of the pending transactions that have just been created into this new block
+		//so that they're inside our blockchain and they cannot be changed
+		//so all awaiting new transactions will be placed in this block
+		transactions: this.pendingTransactions,
+		//this will be equal to nonce parameter that we have passed into this function
+		//A nonce comes from a proof of work. In our case it's simply a number. It could be any number
+		//This nonce is a proof that we created this new block in a legitimate way by using 
+		//a proof of work method.   
+		nonce: nonce,
+		//the hash will be equal to the `hash` parameter that we pass into this function, 
+		//The hash will be the data from our new block. We're going to pass our 
+		//new transactions into a hashing function to output a single string code
+		hash: hash,
+		//this will be equal to the parameter `previousBlockHash` that we pass to this function
+		previousBlockHash: previousBlockHash
+
+	};
+
+	//set the newTransactions to an empty array. Because once we create a new block
+	//we putting all of the new transactions into the new block. So we want to 
+	//clear out this entire `newTransactions` array, so we can start over for the 
+	//next block.
+	this.pendingTransactions = [];
+	//takes the new block that we created and pushes it into our chain, and adds it to our chain.
+	this.chain.push(newBlock);
+
+	return newBlock;
+}
+
+Blockchain.prototype.getLastBlock = function() {
+	return this.chain[this.chain.length - 1];
+}
+
+Blockchain.prototype.createNewTransaction = function(amount, sender, recipient) {
+	const newTransaction = {
+		amount: amount,
+		sender: sender,
+		recipient: recipient
+	};
+
+	this.pendingTransactions.push(newTransaction);
+	//returns the index of the next block (when it's mined) that our new transaction will be stored in
+	return this.getLastBlock()['index'] + 1;
+}
+
+//export Blockchain constructor function to be accessible 
+//by other js files
+module.exports = Blockchain;
---
+++ dev/test.js
@@ -0,0 +1,18 @@
+//import Blockchain constructor function from blockchain.js file
+const Blockchain = require('./blockchain');
+
+//create a new, there no data in it and no blocks
+const testcoin = new Blockchain();
+
+//creating new block. We're just passing in what we want for now
+testcoin.createNewBlock(2899, 'WEQEQEQEQEW', 'RTRTRTRTRTRRTTR');
+
+//create new transaction, which will be added to the pending transaction array
+//because we have not mined or created a new block after creating this 
+//transaction
+testcoin.createNewTransaction(100, 'ALEXSDF34R32Q', 'JENN2343FWDCADSC');
+
+testcoin.createNewBlock(12121221, 'ererereer', 'sdssdsdsdsds');
+//running 'node test.js' will print Blockchain with empty chain 
+//and empty newTrasnactions
+console.log(testcoin.chain[1]);
